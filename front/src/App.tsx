eb2a3b4a41ed596ce640cfe76d5a79d3f589d82f
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2022-09-13 19:16:32 -0300

    preparando home page

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,14 +1,15 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './theme/index.css'
 import { Navbar } from './components'
-import { Info, Profile, Vehiculo } from './pages'
+import { Home, Info, Profile, Vehiculo } from './pages'
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path='/' element={<Profile />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/perfil' element={<Profile />} />
         <Route path='/info' element={<Info />} />
         <Route path='/vehiculo' element={<Vehiculo />} />
       </Routes>
